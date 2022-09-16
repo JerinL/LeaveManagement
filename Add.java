5a796e9a170a2139fa5ca96c44021f7fe312d98b
JerinL <JerinJack111@gmail.com>
2022-09-17 04:31:47 +0530

    ChangedAto500

--- Add.java
+++ Add.java
@@ -6,7 +6,7 @@ public class Add
         int b = 10;
         int c = (a + b);
         System.out.println(c);
-        int a1 = 50;
+        int a1 = 500;
         int b1 = 88;
         int c1 = (a1 - b1);
         System.out.println(c1);
