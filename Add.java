06e1ab56bf76f200079babcee5eaad3611be2f74
JerinL <JerinJack111@gmail.com>
2022-09-17 04:07:10 +0530

    modified'a'and'a1'

--- Add.java
+++ Add.java
@@ -1,11 +1,11 @@
 public class Add
 {
     public static void main(String[] args) {
-        int a = 5;
+        int a = 15;
         int b = 10;
         int c = (a + b);
         System.out.println(c);
-        int a1 = 8;
+        int a1 = 18;
         int b1 = 88;
         int c1 = (a1 - b1);
         System.out.println(c1);
